5fa7ae81062fd2e49a63d13a849eb7f8a1445fad
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-08 10:17:50 +0200

    prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.scss'
 
 function App() {
-  return (
-    <div className="App">
-    <h1>Site with useContext/useReducer</h1>
-    <p>test</p>
-    </div>
-  )
+	return (
+		<div className="App">
+		<h1>Site with useContext/useReducer</h1>
+		<p>test</p>
+		</div>
+	)
 }
 
 export default App
