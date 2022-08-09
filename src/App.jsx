122ad658129465c4864084334eb9b4e3b70661c9
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-09 10:05:53 +0200

    show german nouns

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,31 @@ import { AppContext } from './AppContext';
 import './App.scss';
 
 function App() {
-	const {state, dispatch } = useContext(AppContext);
+	const { state, dispatch } = useContext(AppContext);
 
 	return (
 		<div className="App">
 			<h1>Site with useContext/useReducer</h1>
 			<p>test: {state.count}</p>
 			<div className="buttonArea">
-				<button onClick={() => dispatch({ type: 'decreaseCount'})}>-</button>
-				<button onClick={() => dispatch({ type: 'increaseCount' })}>+</button>
+				<button onClick={() => dispatch({ type: 'decreaseCount' })}>
+					-
+				</button>
+				<button onClick={() => dispatch({ type: 'increaseCount' })}>
+					+
+				</button>
 			</div>
-		<hr/>
-		<p>There are {state.germanNouns.length} nouns.</p>
+			<hr />
+			<p>There are {state.germanNouns.length} nouns.</p>
+			<div className="germanNounArea">
+				{state.germanNouns.map((item) => {
+					return (
+						<div className="germanNoun" key={item.id}>
+							{item.singular}
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
